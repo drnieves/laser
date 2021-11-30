679b9cab22e783d7c3bd736e160c71dced86dd97
drnieves <ernestonievesurosario@gmail.com>
2021-11-30 14:00:40 -0500

    header completado sin particles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-//import Header from "./components/Header";
-//import Particles from "react-particles-js";
+//import Header from "../../laser/src/components/Header";
+import Header from "./components/Header";
+import Particles from "react-particles-js";
 //import AboutMe from "./components/AboutMe";
 //import Services from "./components/Services";
 //import Experience from "./components/Experience";
@@ -12,7 +13,47 @@ import Navbar from "./components/Navbar";
 //import Footer from "./components/Footer";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            color: {
+              value: "#3ca6b1",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            shape: {
+              type: "circle",
+            },
+            number: {
+              value: 30,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
