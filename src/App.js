62cc3a193c149a757dd25a89611d13a89dea532b
drnieves <ernestonievesurosario@gmail.com>
2021-11-30 10:51:19 -0500

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import Navbar from "./components/Navbar";
+//import Header from "./components/Header";
+//import Particles from "react-particles-js";
+//import AboutMe from "./components/AboutMe";
+//import Services from "./components/Services";
+//import Experience from "./components/Experience";
+//import Portfolio from "./components/Portfolio";
+////import Testimonials from "./components/TestimonialsCarousel";
+//import Contacts from "./components/Contacts";
+//import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>hello</h1>;
 }
 
 export default App;
+/* 
+<>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            color: {
+              value: "#3ca6b1",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            shape: {
+              type: "circle",
+            },
+            number: {
+              value: 30,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      
+      <Navbar />
+
+      <Header />
+
+      <AboutMe />
+      <Services />
+      <Experience />
+      <Portfolio />
+      <Testimonials />
+      <Contacts />
+      <Footer />
+    </>
+    */
